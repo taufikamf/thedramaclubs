27bfea572331f3bcc2f6ade211b0c053393cbb41
taufikamf <taufikabdul354@gmail.com>
2023-04-28 11:45:40 +0700

    fixing meta tag

--- pages/posts/[[slug]].vue
+++ pages/posts/[[slug]].vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <Head>
-            <Title>{{postStore.title}}</Title>
-            <Meta name="title" :content="postStore.title"/>
-            <Meta name="description" :content="postStore.description"/>
-            <Meta name="image" :content="postStore.image"/>
-            <Meta name="og:title" :content="postStore.title"/>
-            <Meta name="og:description" :content="postStore.description"/>
-            <Meta name="og:image" :content="postStore.image"/>
+            <Title>{{data[0]?.title.rendered}}</Title>
+            <Meta name="title" :content="data[0]?.title.rendered"/>
+            <Meta name="description" :content="data[0]?.yoast_head_json?.og_description"/>
+            <Meta name="image" :content="yoast_head_json?.og_image[0]?.url"/>
+            <Meta name="og:title" :content="data[0]?.title.rendered"/>
+            <Meta name="og:description" :content="data[0]?.yoast_head_json?.og_description"/>
+            <Meta name="og:image" :content="data[0].yoast_head_json?.og_image[0]?.url"/>
         </Head>
         <section>
             <h1 v-if="postStore.loading">Loading...</h1>
@@ -18,11 +18,14 @@
 </template>
 <script setup>
 import { usePostStore } from '~/stores/PostStore';
+const route = useRoute()
+const runtimeConfig = useRuntimeConfig()
+
+const {data, pending, refresh} = await useFetch(()=>`${runtimeConfig.public.API_URL}wp-json/wp/v2/posts?slug=${route.params.slug}`)
+
 const postStore = usePostStore()
 postStore.getPost()
 
-const runtimeConfig = useRuntimeConfig()
-const route = useRoute()
 
 postStore.getDetailPost(route.params.slug)
 if (process.client && document) {

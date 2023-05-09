03b213c104507ac043616f5fc953c2f84bec3cf7
taufikamf <taufikabdul354@gmail.com>
2023-05-10 00:21:30 +0700

    fix bug again

--- middleware/client.ts
+++ middleware/client.ts
@@ -1,18 +1,31 @@
 export default defineNuxtRouteMiddleware( async(to, from) => {
-    console.log(from)
     const runtimeConfig = useRuntimeConfig()
     const url = to.params
     const fbclid = from.query.fbclid
-    if(url.slug){
-    const res = await fetch(`${runtimeConfig.public.API_URL}wp-json/wp/v2/posts?slug=${url.slug}`)
-    const data = await res.json()
-    navigateTo(data[0].link, {
-        external: true
-    })
+    if(fbclid){
+      if(url.slug){
+        const res = await fetch(`${runtimeConfig.public.API_URL}wp-json/wp/v2/posts?slug=${url.slug}`)
+        const data = await res.json()
+        navigateTo(data[0].link, {
+          external: true
+        })
+      }else{
+        navigateTo(`${runtimeConfig.public.API_URL}`, {
+          external: true
+        })
+      }
     }else{
-    navigateTo(`${runtimeConfig.public.API_URL}`, {
-        external: true
-    })
+        if(url.slug){
+        const res = await fetch(`${runtimeConfig.public.API_URL}wp-json/wp/v2/posts?slug=${url.slug}`)
+        const data = await res.json()
+        navigateTo(data[0].link, {
+            external: true
+        })
+        }else{
+        navigateTo(`${runtimeConfig.public.API_URL}`, {
+            external: true
+        })
+        }
     }
   });
   

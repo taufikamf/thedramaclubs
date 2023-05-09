8299f59834156a7d4481140af759e682fbedd335
taufikamf <taufikabdul354@gmail.com>
2023-05-09 19:45:21 +0700

    fixing bug title

--- middleware/facebook.ts
+++ middleware/facebook.ts
@@ -27,18 +27,6 @@ export default defineNuxtRouteMiddleware( async(to, from) => {
         external: true
       })
     }
-  }else if(from.path != '/'){
-    if(url.slug){
-      const res = await fetch(`${runtimeConfig.public.API_URL}wp-json/wp/v2/posts?slug=${url.slug}`)
-      const data = await res.json()
-      navigateTo(data[0].link, {
-        external: true
-      })
-    }else{
-      navigateTo(`${runtimeConfig.public.API_URL}`, {
-        external: true
-      })
-    }
   }else{
     navigateTo(to.fullPath)
   }
